a38543eee66074f79c1778e5ad6355634bfb37e3
Kaleb Soares <99767124+Tarzskizzy@users.noreply.github.com>
2024-04-12 16:39:19 -0300

    Update simple_GC.h

--- simple_GC.h
+++ simple_GC.h
@@ -6,9 +6,9 @@ typedef struct Node_memoria//lista encadeada que tem armazenado todos os ponteir
     void *Mem; //ponteiro para a memória que foi alocada pelo meu malloc customizado
     struct Node_memoria *prox;//ponteiro para o próximo nó da lista encadeada
 }mem_t;
-
-static mem_t* _HEAD;
 //inicializando a cabeça
+static mem_t* _HEAD;
+
 void *gc_malloc(size_t size);
 void gc_free(void* ptr);
 void gc_free_all();
